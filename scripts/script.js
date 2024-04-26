059fb7956a97760abe1cffb5c0947c4abd4f2fef
Raquelbateman <aliciaraquel1017@gmail.com>
2024-04-26 11:35:40 -0700

    update

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+let gadgetBtn = document.getElementById("gadgetBtn").addEventListener("click", function(){
+    getGk();
+});
+
+let musicBtn = document.getElementById("musicBtn").addEventListener("click", function(){
+    getHistory();
+});
+let gameBtn = document.getElementById("gameBtn").addEventListener("click", function(){
+    getGame();
+});
