63880e99c5c22b2ca5638b5e8a0725081c35076a
Raquelbateman <aliciaraquel1017@gmail.com>
2024-04-26 19:34:39 -0700

    update

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,23 @@ let musicBtn = document.getElementById("musicBtn").addEventListener("click", fun
 let gameBtn = document.getElementById("gameBtn").addEventListener("click", function(){
     getGame();
 });
+
+async function getGadget(){
+    let apiResponse = await fetch("https://opentdb.com/api.php?amount=1&category=9&difficulty=medium&type=boolean").then((Response) => Response.json());
+    console.log(apiResponse);
+    displayArea.innerText = apiResponse.results["0"].question;
+    bodyBack.classList = "bg1";
+}
+
+async function getMusic(){
+    let apiResponse = await fetch("https://opentdb.com/api.php?amount=1&category=23&difficulty=medium&type=boolean").then((Response) => Response.json());
+    console.log(apiResponse);
+    displayArea.innerText = apiResponse.results["0"].question;
+    bodyBack.classList = "bg2";
+}
+async function getGames(){
+    let apiResponse = await fetch("https://opentdb.com/api.php?amount=1&category=15&difficulty=medium&type=boolean").then((Response) => Response.json());
+    console.log(apiResponse);
+    displayArea.innerText = apiResponse.results["0"].question;
+    bodyBack.classList = "bg3";
+}
