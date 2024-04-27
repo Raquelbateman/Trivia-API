6c4e2eea72e426fe221a51d3fe8663d3e8211d08
Raquelbateman <aliciaraquel1017@gmail.com>
2024-04-26 20:17:10 -0700

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,17 @@
 let pickCategory = document.getElementById("pickCategory");
 
+let answerBtn = document.getElementById("answerBtn");
+
+
+
 let cartoonBtn = document.getElementById("cartoonBtn");
 
 let musicBtn = document.getElementById("musicBtn");
 
 let gameBtn = document.getElementById("gameBtn");
+
+
+//event listeners for buttons - it makes them work
 
 cartoonBtn.addEventListener("click", function(){
     getGadget();
@@ -17,6 +24,8 @@ musicBtn.addEventListener("click", function(){
 gameBtn.addEventListener("click", function(){
     getGames();
 });
+
+// calling the api from the opentdb database to get our data
 
 async function getCartoon(){
     let apiResponse = await fetch("https://opentdb.com/api.php?amount=1&category=9&difficulty=medium&type=boolean").then((Response) => Response.json());
