bb2bc057be44bc66a388166b6633c1cc4816910e
Raquelbateman <aliciaraquel1017@gmail.com>
2024-04-26 20:49:42 -0700

    update

--- scripts/script.js
+++ scripts/script.js
@@ -38,10 +38,12 @@ async function getCartoon(){
 async function getMusic(){
     let apiResponse = await fetch("https://opentdb.com/api.php?amount=13&category=12&type=multiple").then((Response) => Response.json());
     console.log(apiResponse);
+    pickCategory.innerText = apiResponse.results["0"].question;
     answerPlace.innerText = "Your Answer:  " + apiResponse.results["0"].correct_answer;
 };
 async function getGames(){
     let apiResponse = await fetch("https://opentdb.com/api.php?amount=13&category=15&type=multiple").then((Response) => Response.json());
     console.log(apiResponse);
+    pickCategory.innerText = apiResponse.results["0"].question;
     answerPlace.innerText = "Your Answer:  " + apiResponse.results["0"].correct_answer;
 };
