1994841aa095916df07adaded4effbe0f9a89897
Raquelbateman <aliciaraquel1017@gmail.com>
2024-04-26 20:14:55 -0700

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 let pickCategory = document.getElementById("pickCategory");
 
-let gadgetBtn = document.getElementById("gadgetBtn");
+let cartoonBtn = document.getElementById("cartoonBtn");
 
 let musicBtn = document.getElementById("musicBtn");
 
 let gameBtn = document.getElementById("gameBtn");
 
-gadgetBtn.addEventListener("click", function(){
+cartoonBtn.addEventListener("click", function(){
     getGadget();
 });
 
@@ -18,19 +18,19 @@ gameBtn.addEventListener("click", function(){
     getGames();
 });
 
-async function getGadget(){
+async function getCartoon(){
     let apiResponse = await fetch("https://opentdb.com/api.php?amount=1&category=9&difficulty=medium&type=boolean").then((Response) => Response.json());
     console.log(apiResponse);
     pickCategory.innerText = apiResponse.results["0"].question;
 }
 
 async function getMusic(){
-    let apiResponse = await fetch("https://opentdb.com/api.php?amount=1&category=23&difficulty=medium&type=boolean").then((Response) => Response.json());
+    let apiResponse = await fetch("https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=boolean").then((Response) => Response.json());
     console.log(apiResponse);
     pickCategory.innerText = apiResponse.results["0"].question;
 }
 async function getGames(){
-    let apiResponse = await fetch("https://opentdb.com/api.php?amount=1&category=15&difficulty=medium&type=boolean").then((Response) => Response.json());
+    let apiResponse = await fetch("https://opentdb.com/api.php?amount=13&category=15&difficulty=medium&type=boolean").then((Response) => Response.json());
     console.log(apiResponse);
     pickCategory.innerText = apiResponse.results["0"].question;
 }
